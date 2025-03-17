88e665f7a12805917e84bbb6d716b39f8b1da0d3
Roman <firomanse@gmail.com>
2025-03-17 14:52:41 +0300

    add computer
    main realization for mini_pc

---
+++ mini_pc/computer.cpp
@@ -0,0 +1,61 @@
+#include "computer.h"
+
+Computer::Computer(const std::string& working_dir)
+{
+  cpu = new Cpu(4);
+  bus = cpu->getBus();
+  ram = new Ram(256);
+  gpu = new Gpu(2);
+  chipset = new Chipset(2);
+
+  bus->link(ram, 'm');
+  bus->link(gpu, 'g');
+  bus->link(chipset, 'i');
+
+  Clock::setSize(256);
+
+  ram->readPreset(working_dir);
+}
+
+void Computer::start()
+{
+  bool is_powered_on = true;
+  for (;;)
+  {
+    cpu->getCommand();
+    is_powered_on = cpu->executeCommand(0);
+    if (!is_powered_on) { break; }
+  }
+}
+
+void Computer::log()
+{
+  bool is_powered_on = true;
+  for (;;)
+  {
+    cpu->getCommand();
+    is_powered_on = cpu->executeCommand(1);
+    if (!is_powered_on) { break; }
+  }
+}
+
+void Computer::debug()
+{
+  bool is_powered_on = true;
+  std::cout << "ram dump" << std::endl;
+  ram->showDump();
+  for (;;)
+  {
+    std::cout << "cpu dump" << std::endl;
+    cpu->showDump();
+    cpu->getCommand();
+    is_powered_on = cpu->executeCommand(1);
+    if (!is_powered_on) { break; }
+
+    std::cout << "enter any key to continue debug" << std::endl;
+    std::string tmp;
+    std::cin >> tmp;
+  }
+  std::cout << "ram dump" << std::endl;
+  ram->showDump();
+}
---
+++ mini_pc/computer.h
@@ -0,0 +1,29 @@
+#ifndef COMPUTER_H
+#define COMPUTER_H
+
+#include "parts/inc/cpu.h"
+#include "parts/inc/bus.h"
+#include "parts/inc/ram.h"
+#include "parts/inc/gpu.h"
+#include "parts/inc/chipset.h"
+
+#include <iostream> //for debug test
+
+class Computer
+{
+public:
+  Computer(const std::string&);
+
+  void start(); //only gpu out
+  void log(); //commands
+  void debug();  //commands and dumps
+
+private:
+  Cpu* cpu;
+  Bus* bus;
+  Ram* ram;
+  Gpu* gpu;
+  Chipset* chipset;
+};
+
+#endif //COMPUTER_H
