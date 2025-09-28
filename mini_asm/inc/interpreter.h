8973b424789daeded5d6016ed3b7294d4cecad24
Roman <firomanse@gmail.com>
2025-09-28 14:06:53 +0300

    add: label translation method to interpreter

--- mini_asm/inc/interpreter.h
+++ mini_asm/inc/interpreter.h
@@ -2,13 +2,26 @@
 #define INTERPRETER_H
 
 #include <iostream>
+#include <vector>
 #include <fstream>
+
+struct DictionaryItem
+{
+  std::string label;
+  int address;
+  int size;
+};
 
 class Interpreter
 {
 public:
   Interpreter(const std::string&);
 
+  bool isStandardCommand(const std::string&);
+  std::string tryToTranslate(const std::string&);
+  std::string toAddress(const int&);
+
+  void toLabels(const std::string&);
   void toAddresses(const std::string&);
   std::string changeCommands(const std::string&);
   void toBinary(const std::string&);
@@ -16,6 +29,7 @@ public:
 
 private:
   std::string working_dir;
+  //std::string working_file;
 };
 
 #endif //INTERPRETER_H
--- mini_asm/src/interpreter.cpp
+++ mini_asm/src/interpreter.cpp
@@ -5,12 +5,170 @@ Interpreter::Interpreter(const std::string& working_dir_)
   working_dir = working_dir_;
 }
 
-void Interpreter::toAddresses(const std::string& working_file)
+bool Interpreter::isStandardCommand(const std::string& command)
+{
+  if (command == "halt"
+   || command == "poweroff"
+   || command == "not"
+   || command == "set"
+   || command == "write"
+   || command == "and"
+   || command == "or"
+   || command == "add"
+   || command == "sub"
+   || command == "jump"
+   || command == "jz"
+   || command == "get"
+   || command == "read"
+   || command == "0"
+   || command == "1")
+  {
+    return true;
+  }
+  return false;
+}
+
+std::string Interpreter::tryToTranslate(const std::string& command)
+{
+  try
+  {
+    int tmp = stoi(command);
+
+    if (tmp >= 0 && tmp < 16)
+    {
+      std::string out = std::to_string(tmp / 8) + ' ';
+      tmp -= (tmp / 8) * 8;
+      out            += std::to_string(tmp / 4) + ' ';
+      tmp -= (tmp / 4) * 4;
+      out            += std::to_string(tmp / 2) + ' ';
+      tmp -= (tmp / 2) * 2;
+      out            += std::to_string(tmp) + ' ';
+      return out;
+    }
+    return "0 0 0 0 ";
+  }
+  catch (...)
+  {
+    return "0 0 0 0 ";
+  }
+}
+
+std::string Interpreter::toAddress(const int& value)
+{
+  if (value < 0 || value >= 256)
+  {
+    return "0 0 0 0 0 0 0 0 "; //error
+  }
+
+  int tmp = value;
+  std::string out = "";
+  for (int i = 128; i > 1; i /= 2)
+  {
+    out += std::to_string(tmp / i) + ' ';
+    tmp -= (tmp / i) * i;
+  }
+  out += std::to_string(tmp) + ' ';
+
+  return out;
+}
+
+void Interpreter::toLabels(const std::string& working_file)
 {
   std::ifstream input_stream;
   std::ofstream output_stream;
 
   input_stream.open(working_dir + "/code/input/" + working_file);
+  output_stream.open(working_dir + "/code/dict/" + working_file);
+
+  std::vector<DictionaryItem> dictionary;
+
+  if (input_stream.is_open())
+  {
+    std::string command;
+    int counter = 0;
+    while (input_stream >> command)
+    {
+      if (command == "label")
+      {
+        input_stream >> command;
+        std::string label = command;
+        input_stream >> command;
+        if (command == "data")
+        {
+          input_stream >> command;
+          DictionaryItem item;
+          item.label = label;
+          item.size = stoi(command);
+          item.address = counter + (item.size - 1);
+          dictionary.push_back(item);
+
+          if (item.size > 1)
+          {
+            output_stream << "0 0 0 0 ";
+          }
+          output_stream << tryToTranslate(item.label);
+
+          counter += item.size;
+        }
+        else
+        {
+          //error
+        }
+      }
+      else if (isStandardCommand(command))
+      {
+        output_stream << command << ' ';
+        ++counter;
+      }
+      else
+      {
+        output_stream << command << ' ';
+        counter += 2;
+      }
+    }
+  }
+  else
+  {
+    std::cout << "file hasn't been opened" << std::endl;
+  }
+
+  input_stream.close();
+  output_stream.close();
+
+  input_stream.open(working_dir + "/code/dict/" + working_file);
+  output_stream.open(working_dir + "/code/label/" + working_file);
+
+  if(!input_stream.is_open())
+  {
+    std::cout << "file hasn't been opened" << std::endl;
+  }
+
+  std::string command;
+  while (input_stream >> command)
+  {
+    if (!isStandardCommand(command))
+    {
+      for (DictionaryItem item : dictionary)
+      {
+        if (item.label == command)
+        {
+          output_stream << toAddress(item.address);
+        }
+      }
+    }
+    else
+    {
+      output_stream << command << ' ';
+    }
+  }
+}
+
+void Interpreter::toAddresses(const std::string& working_file)
+{
+  std::ifstream input_stream;
+  std::ofstream output_stream;
+
+  input_stream.open(working_dir + "/code/label/" + working_file);
   output_stream.open(working_dir + "/code/addr/" + working_file);
 
   if (input_stream.is_open())
@@ -83,6 +241,7 @@ void Interpreter::toBinary(const std::string& working_file)
 
 void Interpreter::work(const std::string& working_file)
 {
+  toLabels(working_file);
   toAddresses(working_file);
   toBinary(working_file);
 }
